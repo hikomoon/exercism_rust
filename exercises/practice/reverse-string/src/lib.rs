f73d3a08585e9fd1f0e9ca445ad460976b472f57
Isaac Wade <hikomoon@outlook.com>
2024-10-18 19:49:50 +0800

    SYNC

--- exercises/practice/reverse-string/src/lib.rs
+++ exercises/practice/reverse-string/src/lib.rs
@@ -1,3 +1,16 @@
+#[cfg(feature = "grapheme")]
+use unicode_segmentation::UnicodeSegmentation;
+
+#[cfg(feature = "grapheme")]
 pub fn reverse(input: &str) -> String {
-    todo!("Write a function to reverse {input}");
+    let mut strs = input.graphemes(true).collect::<Vec<&str>>();
+    strs.reverse();
+    strs.into_iter().collect::<String>()
+}
+
+#[cfg(not(feature = "grapheme"))]
+pub fn reverse(input: &str) -> String {
+    let mut strs = input.chars().collect::<Vec<char>>();
+    strs.reverse();
+    strs.into_iter().collect::<String>()   
 }
